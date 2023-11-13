d751a964089bf4c46a494438aea4fbe48f221b87
cvhub <cv_hub@163.com>
2023-11-13 10:00:54 +0800

    update sam_hq_vit_h

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 # export vit_b encoder
 python3 scripts/export_onnx_encoder_model.py \
-    --sam_checkpoint /home/cvhub/workspace/resources/weights/sam/sam_vit_b_01ec64.pth \
+    --sam_checkpoint /home/cvhub/workspace/resources/weights/sam/sam_hq_vit_b.pth \
     --output /home/cvhub/workspace/resources/weights/sam/SAM-HQ/sam_hq_vit_b_encoder.onnx \
     --model-type vit_b \
     --quantize-out /home/cvhub/workspace/resources/weights/sam/SAM-HQ/sam_hq_vit_b_encoder_quant.onnx \
@@ -14,7 +14,7 @@ python scripts/export_onnx_model.py \
 
 # export vit_l encoder
 python3 scripts/export_onnx_encoder_model.py \
-    --sam_checkpoint /home/cvhub/workspace/resources/weights/sam/sam_vit_l_0b3195.pth \
+    --sam_checkpoint /home/cvhub/workspace/resources/weights/sam/sam_hq_vit_l.pth \
     --output /home/cvhub/workspace/resources/weights/sam/SAM-HQ/sam_hq_vit_l_encoder.onnx \
     --model-type vit_l \
     --quantize-out /home/cvhub/workspace/resources/weights/sam/SAM-HQ/sam_hq_vit_l_encoder_quant.onnx \
@@ -25,6 +25,20 @@ python scripts/export_onnx_model.py \
     --checkpoint /home/cvhub/workspace/resources/weights/sam/SAM-HQ/sam_hq_vit_l.pth \
     --model-type vit_l \
     --output /home/cvhub/workspace/resources/weights/sam/SAM-HQ/sam_hq_vit_l_decoder.onnx
+
+# export vit_h encoder
+python3 scripts/export_onnx_encoder_model.py \
+    --sam_checkpoint /home/cvhub/workspace/resources/weights/sam/SAM-HQ/sam_hq_vit_h.pth \
+    --output /home/cvhub/workspace/resources/weights/sam/SAM-HQ/sam_hq_vit_h_encoder.onnx \
+    --model-type vit_h \
+    --quantize-out /home/cvhub/workspace/resources/weights/sam/SAM-HQ/sam_hq_vit_h_encoder_quant.onnx \
+    --use-preprocess
+
+# export vit_h decoder
+python scripts/export_onnx_model.py \
+    --checkpoint /home/cvhub/workspace/resources/weights/sam/SAM-HQ/sam_hq_vit_h.pth \
+    --model-type vit_h \
+    --output /home/cvhub/workspace/resources/weights/sam/SAM-HQ/sam_hq_vit_h_decoder.onnx
 
 # inference vit_b
 python scripts/main.py \
@@ -48,4 +62,16 @@ python scripts/main.py \
 python scripts/main.py \
     --encoder_model /home/cvhub/workspace/resources/weights/sam/SAM-HQ/sam_hq_vit_l_encoder_quant.onnx \
     --decoder_model /home/cvhub/workspace/resources/weights/sam/SAM-HQ/sam_hq_vit_l_decoder.onnx \
+    --img_path /home/cvhub/workspace/projects/python/sam/sam-hq/demo/input_imgs/example4.png
+
+# inference vit_h
+python scripts/main.py \
+    --encoder_model /home/cvhub/workspace/resources/weights/sam/SAM-HQ/sam_hq_vit_h_encoder.onnx \
+    --decoder_model /home/cvhub/workspace/resources/weights/sam/SAM-HQ/sam_hq_vit_h_decoder.onnx \
+    --img_path /home/cvhub/workspace/projects/python/sam/sam-hq/demo/input_imgs/example4.png
+
+# inference vit_h_quant
+python scripts/main.py \
+    --encoder_model /home/cvhub/workspace/resources/weights/sam/SAM-HQ/sam_hq_vit_h_encoder_quant.onnx \
+    --decoder_model /home/cvhub/workspace/resources/weights/sam/SAM-HQ/sam_hq_vit_h_decoder.onnx \
     --img_path /home/cvhub/workspace/projects/python/sam/sam-hq/demo/input_imgs/example4.png
